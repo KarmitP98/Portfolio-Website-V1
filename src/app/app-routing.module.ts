5ad25537191d587d247c63848f5bfe86d8ccc936
Karmit Personal <karmit199@gmail.com>
2021-03-24 00:15:02 -0400

    [Feat] Added Contact Page. [Todo] Add Contact Input, add work page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule}             from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ContactComponent}     from './components/contact/contact.component';
+import {DashboardComponent}   from './components/dashboard/dashboard.component';
+import {WorkComponent}        from './components/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'work', component: WorkComponent},
+  {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+            imports: [RouterModule.forRoot(routes)],
+            exports: [RouterModule]
+          })
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { Project1Component } from './components/projects/project1/project1.component';
-import { Project2Component } from './components/projects/project2/project2.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { FabComponent } from './components/fab/fab.component';
+import { AppRoutingModule }   from './app-routing.module';
+import { AppComponent }       from './app.component';
+import {AboutMeComponent}     from './components/dashboard/about-me/about-me.component';
+import { ProjectsComponent }  from './components/dashboard/projects/projects.component';
+import { Project1Component }  from './components/dashboard/projects/project1/project1.component';
+import { Project2Component }  from './components/dashboard/projects/project2/project2.component';
+import { FooterComponent }    from './components/footer/footer.component';
+import { FabComponent }       from './components/fab/fab.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { WorkComponent }      from './components/work/work.component';
+import { ContactComponent }   from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/dashboard/hero/hero.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { FabComponent } from './components/fab/fab.component';
     Project1Component,
     Project2Component,
     FooterComponent,
-    FabComponent
+    FabComponent,
+    AboutMeComponent,
+    DashboardComponent,
+    WorkComponent,
+    ContactComponent,
+    NavbarComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
