e234b6af545a0723bc921ab251e990144fa0ad3e
Karmit Personal <karmit199@gmail.com>
2021-03-24 16:46:07 -0400

    [Feat] Finished Contacts page. [Todo] Connect links and add data. Add Contact page.

---
+++ src/app/components/work/github-portfolio/github-portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-portfolio',
+  templateUrl: './github-portfolio.component.html',
+  styleUrls: ['./github-portfolio.component.scss']
+})
+export class GithubPortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/work/hero-work/hero-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-work',
+  templateUrl: './hero-work.component.html',
+  styleUrls: ['./hero-work.component.scss']
+})
+export class HeroWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
